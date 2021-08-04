7556b76ce4539dbf6a4986431f0c46ab995f82e3
tawakl <tawakl@ouredu.com>
2021-08-04 15:16:53 +0200

    logo

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,6 @@
             require base_path('app/Starter/Governorates/Routes/web.php');
             require base_path('app/Starter/Policy/Routes/web.php');
             require base_path('app/Starter/ContactUs/Routes/web.php');
-//            Route::get('/', 'DashBoardController@getIndex');
+            Route::get('/', '\App\Starter\Users\Controllers\UsersController@getIndex');
         });
 });
